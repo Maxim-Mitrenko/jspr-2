c8274b63c374137873ade05c905371e54f97b1bc
Maxim-Mitrenko <klevaya.farm@gmail.com>
2022-07-13 19:50:05 +0300

    Add concurrent collection

--- src/main/java/repository/PostRepository.java
+++ src/main/java/repository/PostRepository.java
@@ -4,13 +4,13 @@ import exception.NotFoundException;
 import model.Post;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @Repository
 public class PostRepository {
-    private final List<Post> posts = new ArrayList<>();
+    private final List<Post> posts = new CopyOnWriteArrayList<>();
     private int size = 0;
 
     public List<Post> all() {
