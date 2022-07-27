b55500d343895af338f23da68e859233c0fd5915
Maxim-Mitrenko <klevaya.farm@gmail.com>
2022-07-27 12:27:06 +0300

    Update PostRepository

--- src/main/java/repository/PostRepository.java
+++ src/main/java/repository/PostRepository.java
@@ -3,40 +3,39 @@ package repository;
 import exception.NotFoundException;
 import model.Post;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class PostRepository {
-    private final List<Post> posts = new CopyOnWriteArrayList<>();
+
+    private final Map<Long, Post> posts = new ConcurrentHashMap<>();
     private final AtomicInteger size = new AtomicInteger();
 
     public List<Post> all() {
-        return posts;
+        return new ArrayList<>(posts.values());
     }
 
     public Optional<Post> getById(long id) {
-        return posts.stream()
-                .filter(x -> x.getId() == id)
-                .findAny();
+        return Optional.ofNullable(posts.get(id));
     }
 
     public Post save(Post post) {
         if (post.getId() == 0) {
             post.setId(size.addAndGet(1));
-            posts.add(post);
+            posts.put(post.getId(), post);
         } else {
-            final var found = getById(post.getId()).orElseThrow(NotFoundException::new);
-            final var index = posts.indexOf(found);
-            posts.remove(found);
-            posts.add(index, post);
+            getById(post.getId()).orElseThrow(NotFoundException::new);
+            posts.put(post.getId(), post);
         }
         return post;
     }
 
     public void removeById(long id) {
-        final var post = getById(id).orElseThrow(NotFoundException::new);
-        posts.remove(post);
+        var result = posts.remove(id);
+        if (result == null) throw new NotFoundException();
     }
 }
